4c4a4909ba84c5371c84835bff0ce4cff2a4fcf5
Oliver Drobnik <oliver@drobnik.com>
2011-09-02 16:15:05 +0200

    Moved Files

--- DTWebArchive/main.m
+++ DTWebArchive/main.m
@@ -3,7 +3,7 @@
 //  DTWebArchive
 //
 //  Created by Oliver Drobnik on 9/2/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Cocoanetics. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
